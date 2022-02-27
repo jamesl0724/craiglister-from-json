12a8014d730e8f201de28360bba6d35458e21752
JamesL <jamesdream0724@gmail.com>
2022-02-27 18:47:04 +0700

    chrome driver header fix error

--- clister.py
+++ clister.py
@@ -643,6 +643,7 @@ def craiglister(jsdata):
 
 
     # listing.driver = webdriver.Chrome(chromedriver)
+    chrome_options = webdriver.ChromeOptions()
     chrome_options.add_argument('--headless')
     chrome_options.add_argument('--no-sandbox')
     chrome_options.add_argument('--disable-dev-shm-usage')
